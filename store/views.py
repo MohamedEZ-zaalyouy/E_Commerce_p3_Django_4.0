a8b3af59d1c22d6e72862df3ae438796995c74f1
MohamedEZ-zaalyouy <mohamedezzaalyouy.98@gmail.com>
2023-02-25 13:33:13 +0100

    add urls

--- store/views.py
+++ store/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#############################################
+# Start views Store
+#############################################
+
+
+def store(request):
+    context = {
+
+
+    }
+    return render(request, 'store/store.html', context)
+
+
+#############################################
+# Start views Cart
+#############################################
+
+
+def cart(request):
+    context = {
+
+
+    }
+    return render(request, 'store/cart.html', context)
+
+#############################################
+# Start views Checkout
+#############################################
+
+
+def checkout(request):
+    context = {
+
+
+    }
+    return render(request, 'store/checkout.html', context)
